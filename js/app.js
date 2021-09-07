5619cf608e5c8e568cfaf05aa37da0045505a6e9
Mahmudul Hasan <mahmudulh883@gmail.com>
2021-09-07 15:40:24 +0600

    push

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// const memoryBtn1 = document.getElementById("memory-slot-1");
+// //console.log(memoryBtn1);
+// memoryBtn1.addEventListener("click", function (event) {
+//   console.log("memorySlot");
+// });
+
+const btn = document.querySelector("memory-slot-2");
+btn.addEventListener("click", function (event) {
+  console.log("Button Clicked");
+});
